54db8691878f34e5b9b8ff5cc1fecef0579ecbe5
steven.zolo <42401278+stevenzolo@users.noreply.github.com>
2024-02-27 09:15:42 +0800

    Replicate release version in office windows PC

--- DeepMimicCore/render/Camera.h
+++ DeepMimicCore/render/Camera.h
@@ -2,6 +2,10 @@
 
 #include "util/MathUtil.h"
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 class cCamera
 {
 public:
--- DeepMimicCore/util/MathUtil.h
+++ DeepMimicCore/util/MathUtil.h
@@ -7,6 +7,10 @@
 #include "Eigen/Geometry"
 #include "Rand.h"
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 const int gInvalidIdx = -1;
 
 // for convenience define standard vector for rendering
